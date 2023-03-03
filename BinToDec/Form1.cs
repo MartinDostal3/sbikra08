eabca487603798617480b6bce97b0ac7b13c063e
dostalm.04 <dostalm.04@spst.eu>
2023-03-03 08:40:32 +0100

    Přidejte soubory projektu.

---
+++ BinToDec/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinToDec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int BinToDec(string bin)
+        {
+            int dec = 0;
+            int mocnina = 1;
+            for (int i = bin.Length - 1; i >= 0; i--)
+            {
+                int cifra = int.Parse(bin[i].ToString());
+                dec += cifra * mocnina;
+                mocnina *= 2;
+            }
+            return dec;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bin = textBox1.Text;
+
+            label1.Text = "Cislo decimalne je: " + BinToDec(bin);
+
+        }
+    }
+}
---
+++ DecToBin/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecToBin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string DecToBin(int dec)
+        {
+            if (dec == 0)
+            {
+                return "0";
+            }
+
+
+            string bin = "";
+            while (dec > 0)
+            {
+                int zbytek = dec % 2;
+                bin = zbytek.ToString() + bin;
+                dec /= 2;
+            }
+
+
+            return bin;
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dec = int.Parse(textBox1.Text);
+
+
+            label1.Text = "Číslo binárně je: " + DecToBin(dec);
+
+        }
+    }
+}
